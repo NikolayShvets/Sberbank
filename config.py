30dfa304c0811b05311c82839e95d6ed01847aee
Nikolay Shvets <47063312+NikolayShvets@users.noreply.github.com>
2020-05-19 23:07:56 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = "postgresql://nikonikoni:niko1324@localhost:5432/Sberbank"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "need to change it..."
+LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')

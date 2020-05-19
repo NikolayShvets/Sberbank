4a23a9e973d40da9e49ea389faf0e4677458f381
Nikolay Shvets <47063312+NikolayShvets@users.noreply.github.com>
2020-05-19 21:03:07 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+_basedir = os.path.abspath(os.path.dirname(__name__))
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = "postgresql://nikonikoni:niko1324@localhost:5432/Sberbank"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "need to change it..."
